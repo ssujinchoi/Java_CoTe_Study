ef9b32f446f4f5fbcd16e2b843e8a27466fb4b1c
choisujin <wcsjinn@naver.com>
2021-05-08 00:59:19 +0900

    [chsujin]Kakao19_크레인인형뽑기_수도코드

---
+++ 2주차/Practice/[chsujin]Kakao19_크레인인형뽑기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+// 수도코드
+/* moves횟수만큼 반복문을 돌면서 열의 좌표는 moves로 잡으면 될 것 같은데 행의 좌표를 어떤 순서로 잡아야할지 모르겠다.
+    moves는 1부터 시작하는데 인덱스로 접근해야함으로 moves-1를 해준다.
+	board를 돌면서 0이면 무시한다.
+    바구니는 스택자료구조를 사용한다. 격자(크레인)에서 뽑은 값을 스택에 값(인형)을 넣기 전에 스택에서 peek한 값과 격자에서 뽑은값이 같으면 넣지않고 pop만 한다. 
+    이 과정이 터뜨려 지는 과정이므로 result를 2개씩 증가시켜준다.
+    뽑은 값(인형)의 인덱스에는 0을 넣어준다.
+*/
+class Kakao19_CraneDollGame {
+	public static void main(String[] args) 
+	{
+		Stack<Integer> st = new Stack<>();
+	}
+}
